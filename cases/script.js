76a58f633d30f6b1ba00368858b274ac3f06bfdc
Juliana Viana <julianacpma@hotmail.com>
2022-09-13 15:44:56 -0300

    finalização da home e pagina interna das cases

---
+++ cases/script.js
@@ -0,0 +1,10 @@
+$('.carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: true,
+    freeScroll: true,
+    autoPlay: 2500
+  
+  });
