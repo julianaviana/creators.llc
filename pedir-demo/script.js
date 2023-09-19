0ca12d6bec318738bf89531fc8d7d93af78b3edb
Juliana Viana <julianacpma@hotmail.com>
2023-09-19 18:12:13 -0300

    Inclusão de novas sessões da pagina de agendar demo

--- pedir-demo/script.js
+++ pedir-demo/script.js
@@ -20,6 +20,18 @@ $('.button-group .button--next').on( 'click', function() {
   $carousel.flickity('next');
 });
 
+$('.modalvideo .close').click(function(){
+  $('.modalvideo').css('display','none');
+  $('html').css('overflow-y','auto');
+  $('body > div > video').attr("src","");;
+});
+
+$('.second-sicredi-reels').click(function(){
+  $('.modalvideo').css('display','flex');
+  $('html').css('overflow-y','hidden');
+  $('body > div > video').attr("src","./assets/image/reels-sicredi-02.mp4");
+});
+
 $('body').on('click', '#ff-submit-root', function() {
   gtag('event', 'conversion', {'send_to': 'AW-626645894/hDjiCNqVhtUBEIa356oC'});
 });
